f54e24d90fddb22cabef274f4f6e452a76079cf7
raki123 <rafael.kiesel@web.de>
2022-10-27 00:11:24 +0200

    Another page and a maybe better matching.

--- eval.py
+++ eval.py
@@ -12,6 +12,41 @@ page_locations_and_cells = {
         ((1250,1850), (3,4)), 
         ((1325,1850), (3,5)),
         ((1400,1850), (3,6))
+        ],
+    1 : [
+        ((250,725), (3,7)), 
+        ((310,725), (3,8)),
+        ((360,725), (3,9)),
+        ((410,725), (3,10)), 
+        ((470,725), (3,11)),
+        ((530,725), (3,12)),
+        ((610,725), (3,13)), 
+        ((670,725), (3,14)),
+        ((740,725), (3,15)),
+        ((810,725), (3,16)), 
+        ((870,725), (3,17)),
+        ((930,725), (3,18)),
+        ((1050,725), (3,19)), 
+        ((1110,725), (3,20)),
+        ((1180,725), (3,21)),
+        ((1240,725), (3,22)), 
+        ((1300,725), (3,23)),
+        ((280,1865), (3,24)), 
+        ((340,1865), (3,25)),
+        ((400,1865), (3,26)),
+        ((460,1865), (3,27)), 
+        ((520,1865), (3,28)),
+        ((580,1865), (3,29)),
+        ((640,1865), (3,30)), 
+        ((700,1865), (3,31)),
+        ((760,1865), (3,32)),
+        ((820,1865), (3,33)), 
+        ((880,1865), (3,34)),
+        ((1120,1865), (3,35)),
+        ((1180,1865), (3,36)), 
+        ((1250,1865), (3,37)),
+        ((1320,1865), (3,38)),
+        ((1390,1865), (3,39))
         ]
 }
 
@@ -20,50 +55,54 @@ worksheet = workbook["Answers"]
 
 def find_most_likely(img, location):    
     #Convert it to grayscale
-    img_cropped = img[location[0]:location[0]+HEIGHT, location[1]:location[1]+WIDTH]
-    cv2.imshow('asdf', img_cropped)
+    img_cropped = img[location[0]+20:location[0]+HEIGHT-20, location[1]+30:location[1]+WIDTH-30]
+    cv2.imshow("tested", img_cropped)
     cv2.waitKey()
     img_gray = cv2.cvtColor(img_cropped, cv2.COLOR_BGR2GRAY)
     # img_gray = cv2.GaussianBlur(img_gray,(1,1),0)
-    thresh = 230
-    img_bw = cv2.threshold(img_gray, thresh, 255, cv2.THRESH_BINARY)[1]
-    best = -1
-    best_val = 0
+    # thresh = 230
+    # img_bw = cv2.threshold(img_gray, thresh, 255, cv2.THRESH_BINARY)[1]
+    all_vals = []
     for i in range(1, 7):
         # Read the template
         template = cv2.imread(f'{i}.png', 0)
-        thresh = 150
-        template_bw = cv2.threshold(template, thresh, 255, cv2.THRESH_BINARY)[1]
+        # thresh = 150
+        # template_bw = cv2.threshold(template, thresh, 255, cv2.THRESH_BINARY)[1]
         
         # Store width and height of template in w and h
-        w, h = template.shape[::-1]
+        # w, h = template.shape[::-1]
         
         # Perform match operations.
-        res = cv2.matchTemplate(img_bw, template_bw, cv2.TM_CCOEFF_NORMED)
+        res = cv2.matchTemplate(img_gray, template, cv2.TM_SQDIFF_NORMED)
 
         # Specify a threshold
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
         # Store the coordinates of matched area in a numpy array
         # loc = np.unravel_index(res.argmax(), res.shape)
-        loc = max_loc
-        if max_val > best_val:
-            best = i
-            best_val = max_val
-
+        # loc = max_loc
+        all_vals.append(min_val)
 
         # Draw a rectangle around the matched region.
         # for pt in zip(*loc[::-1]):
         #     print(pt)
-        detected = img_bw.copy()
+        # detected = img_gray.copy()
         # cv2.rectangle(detected, loc, (loc[0] + w, loc[1] + h), (0, 255, 255), 2)
         
-        # # # Show the final image with the matched area.
+        # # # # Show the final image with the matched area.
         # cv2.imshow('Detected', detected)
         # cv2.waitKey()
+    best_val = min(all_vals)
+    best = all_vals.index(best_val) + 1
+    # all_vals[best - 1] = -1
+    # second_best_val = max(all_vals)
+    # print(best_val, best_val - second_best_val, all_vals.index(second_best_val) + 1)
     return best
 
 for p in page_locations_and_cells.keys():
     img = np.array(pages[p])
+    img_half = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+    cv2.imshow('Half Image', img_half)
+    cv2.waitKey()
     for l,c in page_locations_and_cells[p]: 
         match = find_most_likely(img, l)
         print(f"Most likely match: {match}")
